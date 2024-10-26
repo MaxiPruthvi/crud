869d27f6d7500f77dcc2bba79406f872b3ef4bf8
MaxiPruthvi <maxipruthviraj@gmail.com>
2024-10-26 23:57:09 +0530

    false

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def employee(id):
     
     conn.close()
 
-if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=False,host='0.0.0.0')
